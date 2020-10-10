e972c11d62471c3f303ab271f2bdc4c218ab2a5f
unknown <bylasuhaimi@gmail.com>
2020-10-10 22:11:28 +0800

    imported the CalendarModule and also register the plugin in imports array

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,15 @@
+// home.module.ts
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
+// Calendar UI Module
+import { CalendarModule } from 'ion2-calendar';
 
 
 @NgModule({
@@ -12,8 +17,15 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    CalendarModule
   ],
   declarations: [HomePage]
 })
+
 export class HomePageModule {}
